067d9cdd533f542da39a21cf24a02529b2220310
Gytis Trikleris <gytis@redhat.com>
2018-04-25 12:25:00 +0200

    Adapt booster for OSIO

--- .openshiftio/Jenkinsfile.setup.groovy
+++ .openshiftio/Jenkinsfile.setup.groovy
@@ -1,6 +1,5 @@
 
 def setupEnvironmentPre(env) {
-  //sh "oc policy -n ${env} add-role-to-user view -z default"
   sh "if ! oc get -n ${env} configmap app-config -o yaml | grep application.yml; then oc create -n ${env} configmap app-config --from-file=application.yml; fi"
 }
 
